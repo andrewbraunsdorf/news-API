19c03440f004f6792ea177ea0ae56627684e25b5
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-14 21:15:11 +0000

    added basic search function

---
+++ script.js
@@ -0,0 +1,7 @@
+function search() {
+    var xhrCall = new XMLHttpRequest();
+    const keyword = document.getElementById("keyword").value;
+	 console.log(keyword);
+	 const date = document.getElementById("date").value;
+	 console.log(date);
+}
