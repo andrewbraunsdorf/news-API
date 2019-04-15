6fb2a2e9fb6e21efa17db6374177eee7b0b16954
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-04-15 21:57:11 +0000

    added xhr onload

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 function search() {
     var xhrCall = new XMLHttpRequest();
     const keyword = document.getElementById("keyword").value;
-	 console.log(keyword);
-	 const date = document.getElementById("date").value;
-	 console.log(date);
-	 
-        var url = 'https://newsapi.org/v2/everything?' +
-          'q=' + keyword + '&' +
-          'from='+ date+ '&' +
-          'sortBy=popularity&' +
-          'apiKey='+ newsAPIKey;
+    console.log(keyword);
+    const date = document.getElementById("date").value;
+    console.log(date);
+
+    var url = 'https://newsapi.org/v2/everything?' +
+        'q=' + keyword + '&' +
+        'from=' + date + '&' +
+        'sortBy=popularity&' +
+        'apiKey=' + newsAPIKey;
+
+    xhrCall.open('GET', url, true);
+
+    //onload check the readystate automatically, but not the status
+    xhrCall.onload = function() {
+
+        var objectData = JSON.parse(this.responseText);
+        console.log(objectData)
+        document.getElementById('results').innerHTML = ''
+        // data.articles
+    };
 }
